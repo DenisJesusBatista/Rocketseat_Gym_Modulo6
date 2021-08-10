152360c88b314837ef0c15ce18c3393c218d9294
Denis Batista <denis.si2011@gmail.com>
2021-08-10 18:42:37 -0300

    Conhecendo promises.

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,6 +1,24 @@
+const Category = require('../models/Category')
+
 module.exports = {
     create(req, res) {
+        // Pegar categproas
+        Category.all()
+            .then(function (results) {
+
+                const categories = results.rows
+
+                return res.render("products/create.njk", { categories })
+
+            }).catch(function (err) {
+                throw new Error(err)
+            })
+
         return res.render("products/create.njk")
+
+    },
+
+    post(req, res) {
 
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -6,9 +6,15 @@ const ProductController = require('./app/controllers/ProductController')
 routes.get('/', function (req, res) {
 });
 
+
+
+
+
 routes.get('/products/create', ProductController.create)
+routes.post('/products', ProductController.post)
 
 
+//Alias
 routes.get('/ads/create', function (req, res) {
 });
 
