c207e25f59465b9ea80ccd8f06d9de7e4d733341
Denis Batista <denis.si2011@gmail.com>
2021-08-11 19:25:05 -0300

    Criando estratégia de máscara de campo.

--- public/scripts.js
+++ public/scripts.js
@@ -1,20 +1,19 @@
-const input = document.querySelector('input[name="price"]')
-input.addEventListener("keydown", function (e) {
+const Mask = {
+    apply(input, func) {
+        setTimeout(function () {
+            input.value = Mask[func](input.value)
+        }, 1)
 
-
-    setTimeout(function () {
-        let { value } = e.target
-
-
+    },
+    formatBRL(value) {
         value = value.replace(/\D/g, "")
 
         /*Aplicar regra de moeda*/
-        value = new Intl.NumberFormat('pt-BR', {
+        return new Intl.NumberFormat('pt-BR', {
             style: 'currency',
             currency: 'BRL'
         }).format(value / 100)
 
-        e.target.value = value
+    }
+}
 
-    }, 1)
-})
