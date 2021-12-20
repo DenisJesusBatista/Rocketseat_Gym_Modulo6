02bf524bff07c60fad108e83dd9342a1d1cf02d8
Denis Batista <denis.si2011@gmail.com>
2021-12-20 18:11:50 -0300

    Dados para apresentação de produto - BD.

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -6,6 +6,8 @@ module.exports = {
         const year = date.getUTCFullYear()
         const month = `0${date.getUTCMonth() + 1}`.slice(-2)
         const day = `0${date.getUTCDate()}`.slice(-2)
+        const hour = date.getHours()
+        const minutes = date.getMinutes()
 
         //return yyyy-mm-dd
         // return console.log(`${year}-${month}-${day}`)
@@ -13,6 +15,8 @@ module.exports = {
             day,
             month,
             year,
+            hour,
+            minutes,
             iso: `${year}-${month}-${day}`,
             birthDay: `${day}/${month}`,
             format: `${day}/${month}/${year}`
